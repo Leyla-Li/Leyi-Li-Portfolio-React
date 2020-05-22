83295933c103556335ebd7a56968fabf947feec7
leyla li <leylali@qq.com>
2020-05-22 15:53:04 -0700

    finish styling hero component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -54,7 +54,7 @@ class App extends React.Component {
               </Navbar.Collapse>
             </Navbar>
 
-            <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.text} />} />
+            <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
             <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
             <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
