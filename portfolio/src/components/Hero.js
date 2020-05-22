7aa43db6f9b46634bbe639bf88dc04ddd328b47a
leyla li <leylali@qq.com>
2020-05-22 15:26:56 -0700

    import tools into Hero component

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Hero(props){
   return(
