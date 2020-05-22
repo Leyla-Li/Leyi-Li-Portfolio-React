905182ef1ac838966cc2b009b53c3af81b16c0bb
leyla li <leylali@qq.com>
2020-05-22 15:02:50 -0700

    create a homepage page

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -8,7 +8,16 @@ function Footer(){
 
   return(
     <footer className="mt-5">
-      
+      <Container fluid={true}>
+        <Row className="border-top justify-content-between p-3">
+          <Col className="p-0" md={3} sm={12}>
+            Leyi Li
+          </Col>
+          <Col className="p-0 d-flex justify-content-end" md={3}>
+            Portfolio By Leyi Li
+          </Col>  
+        </Row>
+      </Container>
     </footer>  
   );
 }
