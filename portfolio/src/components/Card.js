93acf98abc1493367183c9fdc741b26b54525395
leyla li <leylali@qq.com>
2020-05-26 09:16:24 -0700

    adjust img sizes

--- portfolio/src/components/Card.js
+++ portfolio/src/components/Card.js
@@ -5,7 +5,7 @@ function Card (props){
 
   return(
     <div className="d-inline-block g-card" onClick={e => props.click(props.item)}>
-      <img src={props.item.imgSrc} width="40%" height="60%" />
+      <img src={props.item.imgSrc} alt={props.item.imgSrc} className="g-card-image" />
       {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
     </div> 
   );
