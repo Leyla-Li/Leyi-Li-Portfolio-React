c19f4ab7810ea3d19598deafc002d5d46960e2a2
leyla li <leylali@qq.com>
2020-05-26 12:54:19 -0700

    create form with form groups

--- portfolio/src/pages/ContactPage.js
+++ portfolio/src/pages/ContactPage.js
@@ -23,9 +23,23 @@ class ConatactPage extends React.Component{
     return(
       <div>
         <Title title={this.props.title} />
-        
+
         <ContactContent>
           <Form>
+            <Form.Group>
+              <Form.Label htmlFor="full-name">Full Name</Form.Label>
+              <Form.Control id="full-name" name="name" type="text" value={this.state.name} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control id="email" name="email" type="email" value={this.state.email} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="message">Message</Form.Label>
+              <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} />
+            </Form.Group>
 
           </Form>
         </ContactContent>
