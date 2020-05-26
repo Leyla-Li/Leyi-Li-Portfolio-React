71f4abfb3dbd56c90351c1b9780927bc88ff01e5
leyla li <leylali@qq.com>
2020-05-26 08:48:14 -0700

    use cardinfo component in card

--- portfolio/src/components/Card.js
+++ portfolio/src/components/Card.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import CardInfo from './ CardInfo';
 
 function Card (props){
 
   return(
-    <p>Card is here</p> 
+    <div>
+      <img className="card-img" src={props.item.imgSrc} />
+      {props.item.selected && <CardInfo title={props.item.title} SubTitle={props.item.subTitle} link={props.item.link} />}
+    </div> 
   );
 
 }
