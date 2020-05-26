56ab999b3ea8c50791db143cf6c8151e5696c5e0
leyla li <leylali@qq.com>
2020-05-26 08:07:56 -0700

    add testing content to carousel

--- portfolio/src/components/Carousel.js
+++ portfolio/src/components/Carousel.js
@@ -3,7 +3,15 @@ import React from 'react';
 class Carousel extends React.Component {
   constructor(props){
     super(props);
+    this.state={
 
+    }
+  }
+
+  render(){
+    return(
+      <p>this is Carousel</p>
+    );
   }
 }
 
