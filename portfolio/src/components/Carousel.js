e17efc42bd772b3c94276e6d23438e2db3b71972
leyla li <leylali@qq.com>
2020-05-26 08:22:50 -0700

    add projects info to carousel

--- portfolio/src/components/Carousel.js
+++ portfolio/src/components/Carousel.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import shesCoding from '../assests/shescoding-homepage.PNG';
+import campgroundExplorer from '../assests/campground-explorer-page.PNG';
+import caseHawk from '../assests/casehawk-home-page.PNG';
+
 
 class Carousel extends React.Component {
   constructor(props){
     super(props);
     this.state={
-
+      items: [
+        {
+          id: 0,
+          title: 'She\'s Coding',
+          subTitle: 'A website for women in tech',
+          imgSrc: shesCoding,
+          link: 'http://projects.shescoding.org/#/projects',
+          selected: false
+        },
+        {
+          id: 1,
+          title: 'Campground Explorer',
+          subTitle: 'A website that helps you find campground around you',
+          imgSrc: campgroundExplorer,
+          link: 'https://smores-explorer.herokuapp.com',
+          selected: false
+        },
+        {
+          id: 2,
+          title: 'CaseHawk',
+          subTitle: 'A website for small lawfirms',
+          imgSrc: caseHawk,
+          link: 'https://github.com/JS-401-Final/front-end',
+          selected: false
+        }
+      ]
     }
   }
 
