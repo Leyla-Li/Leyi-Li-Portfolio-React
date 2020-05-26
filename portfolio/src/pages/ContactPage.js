8efa0b7b909b550e7bf21eee8e0afb85650ec6d2
leyla li <leylali@qq.com>
2020-05-26 14:18:06 -0700

    add function to handle changes

--- portfolio/src/pages/ContactPage.js
+++ portfolio/src/pages/ContactPage.js
@@ -19,6 +19,10 @@ class ConatactPage extends React.Component{
     }
   }
 
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
   render(){
     return(
       <div>
@@ -28,17 +32,17 @@ class ConatactPage extends React.Component{
           <Form>
             <Form.Group>
               <Form.Label htmlFor="full-name">Full Name</Form.Label>
-              <Form.Control id="full-name" name="name" type="text" value={this.state.name} />
+              <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
             </Form.Group>
 
             <Form.Group>
               <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control id="email" name="email" type="email" value={this.state.email} />
+              <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
             </Form.Group>
 
             <Form.Group>
               <Form.Label htmlFor="message">Message</Form.Label>
-              <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} />
+              <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange} />
             </Form.Group>
 
             <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
