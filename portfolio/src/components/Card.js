34348d3e21dadcddcb3dde1eb7b0cb4e0724e1ec
leyla li <leylali@qq.com>
2020-05-26 09:08:12 -0700

    edit img size

--- portfolio/src/components/Card.js
+++ portfolio/src/components/Card.js
@@ -4,9 +4,9 @@ import CardInfo from './ CardInfo';
 function Card (props){
 
   return(
-    <div>
-      <img className="card-img" src={props.item.imgSrc} />
-      {props.item.selected && <CardInfo title={props.item.title} SubTitle={props.item.subTitle} link={props.item.link} />}
+    <div className="d-inline-block g-card" onClick={e => props.click(props.item)}>
+      <img src={props.item.imgSrc} width="40%" height="60%" />
+      {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
     </div> 
   );
 
